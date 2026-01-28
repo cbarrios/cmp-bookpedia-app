762a99659f3887516334bed1af55ae59c86620de
Carlos Barrios <lalobarrios16@yahoo.com>
2026-01-27 23:35:56 -0500

    (7) Actual koin setup (di)

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,29 +3,13 @@ package org.example.project
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import io.ktor.client.engine.HttpClientEngine
-import org.example.project.book.data.network.KtorRemoteBookDataSource
-import org.example.project.book.data.repository.DefaultBookRepository
 import org.example.project.book.ui.book_list.BookListScreen
-import org.example.project.book.ui.book_list.BookListViewModel
-import org.example.project.core.data.HttpClientFactory
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
     MaterialTheme {
         BookListScreen(
-            viewModel = remember {
-                BookListViewModel(
-                    bookRepository = DefaultBookRepository(
-                        remoteBookDataSource = KtorRemoteBookDataSource(
-                            httpClient = HttpClientFactory.create(
-                                engine = engine
-                            )
-                        )
-                    )
-                )
-            },
             onBookClick = {}
         )
     }
--- composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
@@ -1,11 +1,10 @@
 package org.example.project
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
-import io.ktor.client.engine.darwin.Darwin
+import org.example.project.di.initKoin
 
-fun MainViewController() = ComposeUIViewController {
-    App(
-        engine = remember { Darwin.create() }
-    )
+fun MainViewController() = ComposeUIViewController(
+    configure = { initKoin() }
+) {
+    App()
 }
